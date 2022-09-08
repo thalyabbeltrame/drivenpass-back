8f6ea6e5be5b9adfada1c2cdd537de67ae8565ba
Thalya Busato Beltrame <beltrame.thalya@gmail.com>
2022-09-08 09:40:57 -0300

    feat: add error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import cors from 'cors';
 import express, { Express, json } from 'express';
 import 'express-async-errors';
 
+import { errorHandler } from './middlewares/errorHandlingMiddleware';
 import { router } from './routers/index';
 
 export const app: Express = express();
 app.use(json());
 app.use(cors());
 app.use(router);
+app.use(errorHandler);
