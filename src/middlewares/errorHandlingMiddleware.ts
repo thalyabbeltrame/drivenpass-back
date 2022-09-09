d2ac49515e5032a68d94da8a590cacee9f9bdae2
Thalya Busato Beltrame <beltrame.thalya@gmail.com>
2022-09-09 08:30:53 -0300

    feat: create users table on database

--- src/middlewares/errorHandlingMiddleware.ts
+++ src/middlewares/errorHandlingMiddleware.ts
@@ -2,10 +2,19 @@ import { NextFunction, Request, Response } from 'express';
 
 import { AppError } from '../utils/AppError';
 
-export function errorHandler(err: Error, _req: Request, res: Response, _next: NextFunction) {
+export function errorHandler(
+  err: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) {
   if (err instanceof AppError) {
-    return res.status(err.statusCode).json({ status: 'error', message: err.message });
+    return res
+      .status(err.statusCode)
+      .json({ status: 'error', message: err.message });
   }
 
-  return res.status(500).json({ status: 'error', message: 'Internal server error' });
+  return res
+    .status(500)
+    .json({ status: 'error', message: 'Internal server error' });
 }
